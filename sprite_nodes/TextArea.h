4b839325db2981b9521d3caf9eb8a7db81d21a69
Cvolton <cvolton@cvolton.eu>
2022-03-13 21:05:02 +0100

    Made TextArea work epicly

--- sprite_nodes/TextArea.h
+++ sprite_nodes/TextArea.h
@@ -10,7 +10,7 @@ namespace gd {
 		PAD(0x58);
 
 	public:
-		static TextArea* create(const char* font, bool unknown,
+		/*static TextArea* create(const char* font, bool unknown,
 			std::string caption, float scale, float width, float height) {
 			__asm {
 				movss xmm1, scale
@@ -21,6 +21,34 @@ namespace gd {
 				bool, std::string)>(
 					base + 0x33270
 					)(font, unknown, caption);
+			__asm add esp, 0x20
+			return pRet;
+		}*/
+
+		/*static TextArea* create(const char* font, bool disableColor,
+			std::string caption, float scale, float width, float height,
+			cocos2d::CCPoint const& anchorPos) {
+			__asm {
+				movss xmm1, scale
+				movss xmm2, width
+				movss xmm3, height
+			}
+			auto pRet = reinterpret_cast<TextArea* (__fastcall*)(const char*,
+				bool, std::string, cocos2d::CCPoint)>(
+					base + 0x33270
+					)(font, disableColor, caption, anchorPos);
+			__asm add esp, 0x20
+			return pRet;
+		}*/
+
+		static TextArea* create(const char* font, bool disableColor,
+			std::string caption, float scale, float width, float height,
+			cocos2d::CCPoint const& anchorPos) {
+			auto pRet = reinterpret_cast<TextArea* (__vectorcall*)(const char*,
+				float, float, float, float,
+				bool, std::string, cocos2d::CCPoint)>(
+					base + 0x33270
+				)(font, 0.f, scale, width, height, disableColor, caption, anchorPos);
 			__asm add esp, 0x20
 			return pRet;
 		}
