f9162a43d151ff0c890d721256354ed4d13e9e89
Cvolton <cvolton@cvolton.eu>
2022-06-21 19:52:25 +0200

    Add GJRewardItem

--- level_nodes/GJRewardItem.h
+++ level_nodes/GJRewardItem.h
@@ -23,6 +23,28 @@ namespace gd {
             bool m_bUnk;
     };
 
+    //i stole this from geode gmd
+    //i would like to applaud myself for consistency between using enum and enum class
+    enum class SpecialRewardItem {
+        FireShard = 0x1,
+        IceShard = 0x2,
+        PoisonShard = 0x3,
+        ShadowShard = 0x4,
+        LavaShard = 0x5,
+        BonusKey = 0x6,
+        Orbs = 0x7,
+        Diamonds = 0x8,
+        CustomItem = 0x9,
+    };
+
+    class GJRewardObject : public cocos2d::CCObject {
+    public:
+        SpecialRewardItem m_specialRewardItem;
+        UnlockType m_unlockType;
+        int m_itemID;
+        int m_total;
+    };
+
 }
 
 #endif
